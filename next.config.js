2362adb7f19abc1ae5a4fae22bedf4d495cd858e
subendusingh63-coder <subendusingh63@gmail.com>
2025-10-11 11:23:03 +0530

    Update and rename next.config.ts to next.config.js

---
+++ next.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    allowedDevOrigins: ["https://*.cloudworkstations.dev"],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
